60189793c7d626e129ac5d43d911b4ae7f0df885
kimsubin-26 <kmoo0759@gmail.com>
2020-07-20 10:41:29 +0900

    로또추첨기 최종완성

--- 로또추첨기/Lotto.js
+++ 로또추첨기/Lotto.js
@@ -4,11 +4,11 @@
 //필 메서드 추가
 //Mapping 맵
 //배열에 뽑아서 매칭하는거
-var 후보군 = Arayy(45).fill().map(function(요소, 인덱스){
-          return 인덱스+1;
-          });
-console.log(후보군);
-
+var 후보군 = Array(45)
+  .fill()
+  .map(function (요소, 인덱스) {
+    return 인덱스 + 1;
+  });
 //숫자를 랜덤하게 섞어 앞에꺼나 뒤에꺼 6개뽑기
 var 셔플 = [];
 while(후보군.length > 0) {
@@ -20,7 +20,6 @@ while(후보군.length > 0) {
     //splice 하나씩 뽑는것 이걸 while문으로 반복해줌
 }
 
-console.log(셔플);
 
 var 보너스 = 셔플[셔플.length -1];
 //0~6까지 순서대로 뽑는거
@@ -32,24 +31,79 @@ var 당첨숫자들 = 셔플.slice(0,6).sort(function(p,c){return p-c;});
 console.log('당첨숫자들', 당첨숫자들, '보너스', 보너스);
 //html tag를 element라고 부른다 결국 html id를 찾는것
 //html tag를 element를 태그이름으로 찾을수도있다. ex) getElementsByTagName
-var 결과창 =document.getElementById('결과창');
 
-for(var i=0; i<당첨숫자들.length; i+=1){
-    //몇초뒤에 동작이 나왔으면 할때 쓰는 메소드(비동기!!!)
-    setTimeout(function 콜백함수(){
-        //클로저(closure)문제 발생해결 (비동기사용시 나타나는 js에 나타남)
+
+//querySelector 이걸로 id, className 다 찾을수있다.
+var 결과창 = document.getElementById('결과창');
+
+function 공색칠(숫자, 결과창){
+    var 공 = document.createElement('div');
+    공.textContent = 숫자;
+    공.style.display = 'inline-block';
+    공.style.border = '1px solid black';
+     //자바스크립트는 -를 빼기로 인식해서 그 부분을 대문자로 바꿔줘야한다. 스타일이용시!
+     //border-radius -> borderRadius
+    공.style.borderRadius = '10px';
+    공.style.width = '20px';
+    공.style.height = '20px';
+    공.style.marginRight = '10px';
+    공.className = '공이름'+숫자;
+    공.style.fontSize = '12px';
+    //공 숫자색
+    var 배경색;
+    if (숫자 <=10) {
+        배경색 = 'red';
         
-         var 공 = document.createElement('div');
-    공.textContent = 당첨숫자들[i];
+    }
+    else if(숫자 <=20){
+        배경색 = 'orange';
+    }
+    else if(숫자 <=30){
+            배경색 = 'yellow'
+     }
+    else if(숫자 <=40){
+            배경색='blue';
+            }
+    else{
+        배경색 = 'green';
+    }
+    공.style.background = 배경색;
     결과창.appendChild(공);
-}, 1000); //밀리초
-        
 }
+    //몇초뒤에 동작이 나왔으면 할때 쓰는 메소드(비동기!!!)
+ setTimeout(function 콜백함수(){
+        //클로저(closure)문제 발생해결 (비동기사용시 나타나는 js에 나타남)
+    공색칠(당첨숫자들[0], 결과창);
+}, 1000); //밀리초
+
+setTimeout(function 콜백함수(){
+     공색칠(당첨숫자들[1], 결과창);
+}, 2000); //밀리초
+
+setTimeout(function 콜백함수(){
+   공색칠(당첨숫자들[2], 결과창);
+}, 3000); //밀리초
+
+setTimeout(function 콜백함수(){
+    공색칠(당첨숫자들[3], 결과창);
+}, 4000); //밀리초
+
+setTimeout(function 콜백함수(){
+     공색칠(당첨숫자들[4], 결과창);
+}, 5000); //밀리초
+
+setTimeout(function 콜백함수(){
+   공색칠(당첨숫자들[5], 결과창);
+}, 6000); //밀리초
+
+setTimeout(function 콜백함수(){
+    //클래스 이름으로 tag를 선택하는것 클래스네임은 여러개 쓸수있어서 배열로 선택해줘야한다.
+var 칸 = document.getElementsByClassName('보너스')[0];
+ 공색칠(보너스, 칸); 
+}, 7000);
+        
+
     
    
-//클래스 이름으로 tag를 선택하는것 클래스네임은 여러개 쓸수있어서 배열로 선택해줘야한다.
-var 보너스칸 = document.getElementsByClassName('보너스')[0];
-var 보너스공 = document.createElement('div');
-    보너스공.textContent = 보너스;
-    보너스칸.appendChild(보너스공);
+
 
